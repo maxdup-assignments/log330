eb6c4bca17d0450a2905e8726803901361c2a393
gcheval <chevalier321@gmail.com>
2015-07-11 12:35:57 -0400

    Traitement partiel de la page arrière

--- js/options.js
+++ js/options.js
@@ -26,31 +26,16 @@ $(function () {
       },
       "plugins" : [
         "contextmenu", "dnd", "search",
-        "state", "types", "wholerow"
-      ]
+        "state", "types", "wholerow", "checkbox"
+      ],
 
     }); 
-    // 7 bind to events triggered on the tree
-    $('#save-folder-tree').on("changed.jstree", function (e, data) {
-      console.log(data.selected);
-    });
-    // 8 interact with the tree - either way is OK
-    $('#test-btn').on('click', function () {
-      $('#save-folder-tree').jstree(true).select_node('child_node_1');
-      $('#save-folder-tree').jstree('select_node', 'child_node_1');
-      $.jstree.reference('#save-folder-tree').select_node('child_node_1');
-    });
-
 });
 
 
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor,
     save_folder_tree: $("#save-folder-tree").jstree(true).get_json('#', { 'flat': true })
   }, function() {
     // Update status to let user know options were saved.
@@ -65,15 +50,10 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true,
     save_folder_tree: ''
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
     $('#save-folder-tree').jstree(true).settings.core.data = items.save_folder_tree;
     $('#save-folder-tree').jstree('refresh');
-    console.log(items.save_folder_tree);
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
--- js/script.js
+++ js/script.js
@@ -1,1 +1,67 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"
+src="https://apis.google.com/js/platform.js"
+$(function () { 
+    $('#save-folder-tree').jstree({
+      "core" : {
+        "animation" : 100,
+        "check_callback" : true,
+        "themes" : { "stripes" : true },
+      },
+      "types" : {
+        "#" : {
+          "max_children" : 2, 
+          "max_depth" : 4, 
+          "valid_children" : ["root"]
+        },
+        "root" : {
+          "valid_children" : ["default"]
+        },
+        "default" : {
+          "valid_children" : ["default","file"]
+        },
+        "file" : {
+          "icon" : "glyphicon glyphicon-file",
+          "valid_children" : []
+        }
+      },
+      "plugins" : [
+        "contextmenu", "dnd", "search",
+        "state", "types", "wholerow"
+      ]
+
+    }); 
+    restore_options();
+
+});
+
+function restore_options() {
+  chrome.storage.sync.get({
+    favoriteColor: 'red',
+    likesColor: true,
+    save_folder_tree: ''
+  }, function(items) {
+    $('#save-folder-tree').jstree(true).settings.core.data = items.save_folder_tree;
+    $('#save-folder-tree').jstree('refresh');
+  });
+}
+
+function saveDefault() {
+	chrome.tabs.getSelected(null, function(tab) {
+	    $('#caught_link').html(tab.url);
+	    $('#caught_link_last_published_date').html(document.lastModified);
+	    $('#caught_link_author').html($(document).find('.infosAuteur').html());
+		
+	});
+	
+}
+function saveAs(path) {
+
+}
+
+
+$('#save-button').on('click' , function(){saveDefault();});
+$('#saveas-button').on('click' , function(){saveDefault();});
+
+$('#options-button').on('click' , function() {
+	chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+});
+
